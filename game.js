ff8322f3719cefedd82d4fd020b726199f601d40
Nicolaquetal <nicolas_bonnet6@hotmail.com>
2022-10-22 17:50:59 +0200

    PVP finished, PVE doesn't work

--- game.js
+++ game.js
@@ -25,9 +25,11 @@ const boardgame = (()=>{
     const cleanBoardandWin = () =>{
       board =["0","1","2","3","4","5","6","7","8"];
       winner ="no";
+      turnCounter =0;
       const err = document.querySelectorAll('.removable');
       err.forEach((z)=>{z.remove();}) 
     }
+
   
     const checkWinner =() =>{
       if ((board[0]==board[1] && board[0]==board[2])||(board[0]==board[3] && board[0]==board[6])){ 
@@ -53,17 +55,22 @@ const player = (Playername, Playersign) =>{
     const getName  = () => Playername;
     const getSign = () => Playersign;
     const getScore = () => score;
-    const raiseScore = (score) => {score+= 1;}
+    const raiseScore = () => {score+= 1;}
     const resetScore = () => { score = 0;};
     return {getName, getScore, getSign, resetScore, raiseScore};
 }
 
 const round = (()=>{
     const bigcase = document.querySelectorAll(".case");
-    const playRoundPVP = (players,turn) =>{
+    const playRoundPVP = (players,firstP,Nbround) =>{
         boardgame.cleanBoardandWin();
-        let currentplayer = players[turn];
+        let firstRounP=firstP;
+        let turn=firstP;
+        let currentRound=0;
+        display.displayRound(currentRound+1,Nbround);
+        let currentplayer = players[firstP];
         bigcase.forEach(c => c.addEventListener('click',(e)=>{
+          if(currentRound<Nbround){
             const pos= parseInt(e.target.id[4]);
             const currentdiv = e.target;
             boardgame.add(currentplayer.getSign(),pos)
@@ -74,61 +81,121 @@ const round = (()=>{
                 currentplayer = players[turn];
             }
             else if (boardgame.getWinner()==="Egality"){
-                console.log("Egality");
-
+                let result ="Egality";
+                currentRound +=1
+                firstRounP=(firstP+currentRound%2)%2;
+                turn=firstRounP;
+                currentplayer = players[turn];
+                if(currentRound==Nbround){
+                  
+                  display.displayEndGameMenu(players[0].getScore(),players[1].getScore(),players[0].getName(),players[1].getName());
+                }
+                else{
+                  display.displayEndRoundMenu(result);
+                  display.displayRound(currentRound+1,Nbround);
+                }
             }
             else {
                 currentplayer.raiseScore();
-                console.log ("The winner is "+currentplayer.getName());
+                display.displayScore(players[0].getScore(),players[1].getScore())
+                let result ="The winner is "+currentplayer.getName();
+                currentRound +=1
+                firstRounP=(firstP+currentRound%2)%2;
+                turn=firstRounP;
+                currentplayer = players[turn];
+                if(currentRound==Nbround){
+                  
+                  display.displayEndGameMenu(players[0].getScore(),players[1].getScore(),players[0].getName(),players[1].getName());
+
+                }
+                else{
+                  display.displayEndRoundMenu(result);
+                  display.displayRound(currentRound+1,Nbround);
+                }
+                
             }
-            }))
-    }
-    const playRoundPVE = (players,turn)=>{
-        boardgame.cleanBoardandWin();
-        let currentplayer = players[turn];
-        if(currentplayer==player2){
-            AI.playAIturn(boardgame.getBoard());
-            currentplayer=player1;
           }
+        }))
+      
+    }
+    const playRoundPVE = (players,firstP,Nbround)=>{
+        boardgame.cleanBoardandWin();
+        let firstRounP=firstP;
+        let turn=firstP;
+        let currentRound=0;
+        let currentplayer = players[firstP];
+
         bigcase.forEach(c => c.addEventListener('click',(e)=>{
-            if (currentplayer==player1){
-                const pos= parseInt(e.target.id[4]);
-                const currentdiv = e.target;
-                boardgame.add(player1.getSign(),pos)
-                boardgame.addtoboard(player1.getSign(),currentdiv)
-                boardgame.checkWinner();
-                if (boardgame.getWinner()==="no"){
-                    turn =(turn+1)%2
-                    currentplayer = players[turn];
-                    AI.playAIturn(boardgame.getBoard());
-                    boardgame.checkWinner();
-                    if (boardgame.getWinner()==="no"){
-                        turn =(turn+1)%2
-                        currentplayer = players[turn];
-                    }
-                    else if (boardgame.getWinner()==="Egality"){
-                        console.log("Egality");
-                    }
-                    else {
-                        currentplayer.raiseScore();
-                        console.log ("The winner is "+currentplayer.getName());
-                    }
-                }
-                else if (boardgame.getWinner()==="Egality"){
-                    console.log("Egality");
-                }
-                else {
-                    currentplayer.raiseScore();
-                    console.log ("The winner is "+currentplayer.getName());
-                }
+          console.log("firstround n°"+currentRound+" firstroundP "+firstRounP);
+          if(firstRounP== 1 && currentRound<Nbround){
+            console.log("if1");
+            AI.playAIturn(boardgame.getBoard(),players[turn]);
+            firstRounP=0;
+            turn =0;
+            currentplayer = players[turn];
             }
+          if (turn ==0 && currentRound<Nbround){
+            console.log("if2");
+              const pos= parseInt(e.target.id[4]);
+              const currentdiv = e.target;
+              boardgame.add(players[0].getSign(),pos)
+              boardgame.addtoboard(players[0].getSign(),currentdiv)
+              boardgame.checkWinner();
+              if (boardgame.getWinner()==="no"){
+                console.log("if3");
+                  turn =(turn+1)%2
+                  currentplayer = players[turn];
+                  AI.playAIturn(boardgame.getBoard(),players[turn]);
+                  boardgame.checkWinner();
+                  if (boardgame.getWinner()==="no"){
+                    console.log("if4");
+                      turn =(turn+1)%2
+                      currentplayer = players[turn];
+                  }
+                  else if (boardgame.getWinner()==="Egality"){
+                    console.log("if5");
+                      console.log("Egality");
+                      currentRound +=1
+                      firstRounP=(firstP+currentRound%2)%2;
+                      turn=firstRounP;
+                      display.displayEndRoundMenu("Egality");
+                      
+                  }
+                  else {
+                    console.log("if6");
+                      currentplayer.raiseScore();
+                      console.log ("The winner is "+currentplayer.getName());
+                      currentRound +=1
+                      firstRounP=(firstP+currentRound%2)%2;
+                      turn=firstRounP;
+                      boardgame.cleanBoardandWin();
+                  }
+              }
+              else if (boardgame.getWinner()==="Egality"){
+                console.log("if7");
+                  console.log("Egality");
+                  currentRound +=1
+                  firstRounP=(firstP+currentRound%2)%2;
+                  turn=firstRounP;
+                  boardgame.cleanBoardandWin();
+              }
+              else {
+                console.log("if8");
+                  currentplayer.raiseScore();
+                  console.log ("The winner is "+currentplayer.getName());
+                  currentRound +=1
+                  firstRounP=(firstP+currentRound%2)%2;
+                  turn=firstRounP;
+                  boardgame.cleanBoardandWin();
+              }
+          }
         }))
     }
     return {playRoundPVE, playRoundPVP};
 })();
 
 const AI = (() =>{
-    const playAIturn = (currentboardgame)=>{
+    const playAIturn = (currentboardgame,player2)=>{
         z = StrategyAI1(currentboardgame);
         const currentdiv2 = document.getElementById("case"+z);
         boardgame.add(player2.getSign(),z);
@@ -152,8 +219,108 @@ const AI = (() =>{
     return {playAIturn};
 })();
 
-let turn = 0;
+const game = (()=>{
+
+  const launchGame = (nameP1,nameP2, type, firstP, Nbround)=>{
+    let turn = firstP;
+    const player1 = player(nameP1, "X");
+    const player2 = player(nameP2, "O");
+    const players = [player1,player2];
+    display.displayScore(players[0].getScore(),players[1].getScore())
+    if(type==="PVP"){
+      round.playRoundPVP(players,turn, Nbround);
+    }
+    else{
+      round.playRoundPVE(players,turn, Nbround);
+    }
+  }
+ 
+return{launchGame}
+})();
+
+
+const display = (()=>{
+
+  const displayMainMenu = ()=>{
+    let typeSelected ="PVP";
+    const menuBTN = document.querySelector(".menu");
+    menuBTN.addEventListener('click',openForm)
+    const closeBTN = document.querySelector(".closeMenu");
+    closeBTN.addEventListener('click',closeForm)
+    const radioPVE = document.querySelector("#PVE")
+    radioPVE.addEventListener('change',PVEchecked)
+    const radioPVP = document.querySelector("#PVP")
+    radioPVP.addEventListener('change',PVEunchecked)
+    const playBTN = document.querySelector("#play");
+    playBTN.addEventListener('click',play)
+
+    function play (){
+      const name1 = document.querySelector("#p1").value
+      const name2 = document.querySelector("#p2").value
+      /*const firstp = document.querySelector("#p2").value*/
+      const roundnb = document.querySelector("#rounds").value
+      displayName(name1,name2);
+      game.launchGame(name1,name2 ,typeSelected,0,roundnb);
+      document.querySelector(".form").style.display = "none"; 
+    }
+
+    function openForm() {
+      document.querySelector(".form").style.display = "flex";   
+    }
+    function closeForm() {
+      document.querySelector(".form").style.display = "none";  
+    }
+    function PVEchecked(){
+      document.querySelector(".levelAI").style.display = "flex";
+      document.querySelector("#p2").value="AI"
+      typeSelected ="PVE";
+    }
+    function PVEunchecked(){
+      document.querySelector(".levelAI").style.display = "none";
+      typeSelected ="PVP";
+    }
+  }
+  const displayEndRoundMenu = (result)=>{
+    document.querySelector(".resultatRound").textContent=result;
+    document.querySelector(".endRoundMenu").style.display = "flex";
+    const menuBTNRound = document.querySelector("#btnround");
+    menuBTNRound.addEventListener('click',()=>{
+      boardgame.cleanBoardandWin();
+      document.querySelector(".endRoundMenu").style.display = "none";})
+  }
+
+  const displayEndGameMenu = (P1score,P2score,P1name,P2name)=>{
+    let result="Egality";
+    if(P1score>P2score){result="The winner is "+P1name};
+    if(P1score<P2score){result="The winner is "+P2name}
+    document.querySelector(".resultatGame").textContent=result;
+    document.querySelector(".endGameMenu").style.display = "flex";
+    const menuBTNRound = document.querySelector("#btngame");
+    menuBTNRound.addEventListener('click',()=>{
+      boardgame.cleanBoardandWin();
+      document.querySelector(".endGameMenu").style.display = "none";})
+  }
+  const displayName = (name1,name2) =>{
+    document.querySelector("#nameP1").textContent=name1;
+    document.querySelector("#nameP2").textContent=name2;
+  }
+  const displayScore =(score1,score2)=>{
+    document.querySelector("#scoreP1").textContent=score1;
+    document.querySelector("#scoreP2").textContent=score2; 
+  }
+  const displayRound =(currentRound,Nbround)=>{
+    document.querySelector("#roundcount").textContent="ROUND" +currentRound+"/"+Nbround;
+  }
+  return {displayMainMenu,displayEndRoundMenu,displayEndGameMenu,displayName,displayScore,displayRound}
+})(); 
+display.displayMainMenu();
+
+/*game.launchGame("Xavier","Olivier" ,"PVP",0,2);*/
+
+
+/*
+let turn = 1;
 const player1 = player("Xavier" , "X" );
 const player2 = player("Olivier" , "O");
 const players = [player1,player2];
-round.playRoundPVE(players,0);
+round.playRoundPVE(players,turn);*/
