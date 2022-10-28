6ec553542ff6fb031510a671d8f7f3ef8b31eb08
Nicolaquetal <nicolas_bonnet6@hotmail.com>
2022-10-28 20:32:13 +0200

    add check if case already played

--- game.js
+++ game.js
@@ -4,7 +4,8 @@ const boardgame = (()=>{
     let turnCounter =0;
   
     const add =(sign,pos)=>{
-      board[pos]=sign;
+      if(board[pos]!="X" &&board[pos]!="O"){
+      board[pos]=sign;}
       
     }
     const getBoard = () =>{
@@ -15,11 +16,12 @@ const boardgame = (()=>{
     }
   
     const addtoboard = (sign,curdiv)=>{
-      const ca = document.createElement('div');
-      ca.classList.add('removable');
-      var newContent = document.createTextNode(sign);
-      ca.appendChild(newContent);
-      curdiv.appendChild(ca);
+      if (curdiv.childNodes.length == 0){
+        const ca = document.createElement('div');
+        ca.classList.add('removable');
+        var newContent = document.createTextNode(sign);
+        ca.appendChild(newContent);
+        curdiv.appendChild(ca);}
     }
    
     const cleanBoardandWin = () =>{
@@ -64,6 +66,7 @@ const round = (()=>{
     const bigcase = document.querySelectorAll(".case");
     const playRoundPVP = (players,firstP,Nbround) =>{
         boardgame.cleanBoardandWin();
+        let Inplay=true;
         let firstRounP=firstP;
         let turn=firstP;
         let currentRound=0;
@@ -73,8 +76,15 @@ const round = (()=>{
           if(currentRound<Nbround){
             const pos= parseInt(e.target.id[4]);
             const currentdiv = e.target;
-            boardgame.add(currentplayer.getSign(),pos)
-            boardgame.addtoboard(currentplayer.getSign(),currentdiv)
+            if(e.target.classList =="removable" ){
+              Inplay=false;
+              
+            }
+            else{
+              boardgame.add(currentplayer.getSign(),pos)
+              boardgame.addtoboard(currentplayer.getSign(),currentdiv)
+              Inplay=true;
+            }
             boardgame.checkWinner();
             if (boardgame.getWinner()==="no"){
                 turn =(turn+1)%2
@@ -91,7 +101,7 @@ const round = (()=>{
                   display.displayEndGameMenu(players[0].getScore(),players[1].getScore(),players[0].getName(),players[1].getName());
                 }
                 else{
-                  display.displayEndRoundMenu(result);
+                  display.displayEndRoundMenu(result,0," ");
                   display.displayRound(currentRound+1,Nbround);
                 }
             }
@@ -109,7 +119,7 @@ const round = (()=>{
 
                 }
                 else{
-                  display.displayEndRoundMenu(result);
+                  display.displayEndRoundMenu(result,0," ");
                   display.displayRound(currentRound+1,Nbround);
                 }
                 
@@ -119,74 +129,142 @@ const round = (()=>{
       
     }
     const playRoundPVE = (players,firstP,Nbround)=>{
+      
         boardgame.cleanBoardandWin();
+        let Inplay=true;
         let firstRounP=firstP;
         let turn=firstP;
         let currentRound=0;
+        display.displayRound(currentRound+1,Nbround);
         let currentplayer = players[firstP];
-
+        if(firstRounP== 1 && currentRound==0){
+          console.log("if1");
+          AI.playAIturn(boardgame.getBoard(),players[turn]);
+          turn =0;
+          currentplayer = players[turn];
+          console.log("aapres 1 tour AI " +boardgame.getBoard())
+          }
+        
         bigcase.forEach(c => c.addEventListener('click',(e)=>{
-          console.log("firstround n°"+currentRound+" firstroundP "+firstRounP);
-          if(firstRounP== 1 && currentRound<Nbround){
-            console.log("if1");
-            AI.playAIturn(boardgame.getBoard(),players[turn]);
-            firstRounP=0;
-            turn =0;
-            currentplayer = players[turn];
-            }
-          if (turn ==0 && currentRound<Nbround){
-            console.log("if2");
-              const pos= parseInt(e.target.id[4]);
-              const currentdiv = e.target;
-              boardgame.add(players[0].getSign(),pos)
-              boardgame.addtoboard(players[0].getSign(),currentdiv)
+          
+          if (currentRound<Nbround){
+              
+              
+                const pos= parseInt(e.target.id[4]);
+                const currentdiv = e.target;
+                if(e.target.classList =="removable" ){
+                  Inplay=false;
+                  /*display.displayCaseOccupied();*/
+                  
+                }
+                else{
+                  boardgame.add(players[0].getSign(),pos)
+                  boardgame.addtoboard(players[0].getSign(),currentdiv)
+                  Inplay=true;
+                }
+              
               boardgame.checkWinner();
-              if (boardgame.getWinner()==="no"){
-                console.log("if3");
-                  turn =(turn+1)%2
+              if (boardgame.getWinner()==="no" && Inplay){
+                
+                  turn =(turn+1)%2 
                   currentplayer = players[turn];
+                  
                   AI.playAIturn(boardgame.getBoard(),players[turn]);
                   boardgame.checkWinner();
                   if (boardgame.getWinner()==="no"){
-                    console.log("if4");
                       turn =(turn+1)%2
                       currentplayer = players[turn];
                   }
                   else if (boardgame.getWinner()==="Egality"){
-                    console.log("if5");
-                      console.log("Egality");
-                      currentRound +=1
-                      firstRounP=(firstP+currentRound%2)%2;
-                      turn=firstRounP;
-                      display.displayEndRoundMenu("Egality");
+                    let result ="Egality";
+                    currentRound +=1
+                    firstRounP=(firstP+currentRound%2)%2;
+                    turn=firstRounP;
+                    currentplayer = players[turn];
+                    if(currentRound==Nbround){
+                      display.displayEndGameMenu(players[0].getScore(),players[1].getScore(),players[0].getName(),players[1].getName());
+                    }
+                    else{
+                      if((currentRound%2==0 && firstP==1)||(currentRound%2==1 && firstP==0)){
+                        display.displayEndRoundMenu(result,1,players[1])
+                        turn+=1
+                      }
+                      else{
+                        display.displayEndRoundMenu(result,0," ");
+                      }
+                      display.displayRound(currentRound+1,Nbround);
+                    }
                       
                   }
                   else {
-                    console.log("if6");
-                      currentplayer.raiseScore();
-                      console.log ("The winner is "+currentplayer.getName());
-                      currentRound +=1
-                      firstRounP=(firstP+currentRound%2)%2;
-                      turn=firstRounP;
-                      boardgame.cleanBoardandWin();
+                    currentplayer.raiseScore();
+                    display.displayScore(players[0].getScore(),players[1].getScore())
+                    let result ="The winner is "+currentplayer.getName();
+                    currentRound +=1
+                    firstRounP=(firstP+currentRound%2)%2;
+                    turn=firstRounP;
+                    currentplayer = players[turn];
+                    if(currentRound==Nbround){
+                      
+                      display.displayEndGameMenu(players[0].getScore(),players[1].getScore(),players[0].getName(),players[1].getName());
+
+                    }
+                    else{
+                      if((currentRound%2==0 && firstP==1)||(currentRound%2==1 && firstP==0)){
+                        display.displayEndRoundMenu(result,1,players[1])
+                        turn+=1
+                      }
+                      else{
+                        display.displayEndRoundMenu(result,0," ");
+                      }
+                      display.displayRound(currentRound+1,Nbround);
+                    }
                   }
               }
-              else if (boardgame.getWinner()==="Egality"){
-                console.log("if7");
-                  console.log("Egality");
-                  currentRound +=1
-                  firstRounP=(firstP+currentRound%2)%2;
-                  turn=firstRounP;
-                  boardgame.cleanBoardandWin();
+              else if (boardgame.getWinner()==="Egality" && Inplay){
+                let result ="Egality";
+                currentRound +=1
+                firstRounP=(firstP+currentRound%2)%2;
+                turn=firstRounP;
+                currentplayer = players[turn];
+                if(currentRound==Nbround){
+                  
+                  display.displayEndGameMenu(players[0].getScore(),players[1].getScore(),players[0].getName(),players[1].getName());
+                }
+                else{
+                  if((currentRound%2==0 && firstP==1)||(currentRound%2==1 && firstP==0)){
+                    display.displayEndRoundMenu(result,1,players[1])
+                    turn+=1
+                  }
+                  else{
+                    display.displayEndRoundMenu(result,0," ");
+                  }
+                  display.displayRound(currentRound+1,Nbround);
+                }
               }
-              else {
-                console.log("if8");
-                  currentplayer.raiseScore();
-                  console.log ("The winner is "+currentplayer.getName());
-                  currentRound +=1
-                  firstRounP=(firstP+currentRound%2)%2;
-                  turn=firstRounP;
-                  boardgame.cleanBoardandWin();
+              else if(Inplay){
+                currentplayer.raiseScore();
+                display.displayScore(players[0].getScore(),players[1].getScore())
+                let result ="The winner is "+currentplayer.getName();
+                currentRound +=1
+                firstRounP=(firstP+currentRound%2)%2;
+                turn=firstRounP;
+                currentplayer = players[turn];
+                if(currentRound==Nbround){
+                  
+                  display.displayEndGameMenu(players[0].getScore(),players[1].getScore(),players[0].getName(),players[1].getName());
+
+                }
+                else{
+                  if((currentRound%2==0 && firstP==1)||(currentRound%2==1 && firstP==0)){
+                    display.displayEndRoundMenu(result,1,players[1])
+                    turn+=1
+                  }
+                  else{
+                    display.displayEndRoundMenu(result,0," ");
+                  }
+                  display.displayRound(currentRound+1,Nbround);
+                }
               }
           }
         }))
@@ -242,7 +320,7 @@ return{launchGame}
 const display = (()=>{
 
   const displayMainMenu = ()=>{
-    let typeSelected ="PVP";
+    let typeSelected ="PVE";
     const menuBTN = document.querySelector(".menu");
     menuBTN.addEventListener('click',openForm)
     const closeBTN = document.querySelector(".closeMenu");
@@ -260,7 +338,7 @@ const display = (()=>{
       /*const firstp = document.querySelector("#p2").value*/
       const roundnb = document.querySelector("#rounds").value
       displayName(name1,name2);
-      game.launchGame(name1,name2 ,typeSelected,0,roundnb);
+      game.launchGame(name1,name2 ,typeSelected,1,roundnb);
       document.querySelector(".form").style.display = "none"; 
     }
 
@@ -280,13 +358,15 @@ const display = (()=>{
       typeSelected ="PVP";
     }
   }
-  const displayEndRoundMenu = (result)=>{
+  const displayEndRoundMenu = (result,AIbegin,player)=>{
     document.querySelector(".resultatRound").textContent=result;
     document.querySelector(".endRoundMenu").style.display = "flex";
     const menuBTNRound = document.querySelector("#btnround");
     menuBTNRound.addEventListener('click',()=>{
       boardgame.cleanBoardandWin();
-      document.querySelector(".endRoundMenu").style.display = "none";})
+      document.querySelector(".endRoundMenu").style.display = "none";
+      AIbegin == 1 ? AI.playAIturn(["0","1","2","3","4","5","6","7","8"],player):console.log('');
+    })
   }
 
   const displayEndGameMenu = (P1score,P2score,P1name,P2name)=>{
@@ -300,6 +380,15 @@ const display = (()=>{
       boardgame.cleanBoardandWin();
       document.querySelector(".endGameMenu").style.display = "none";})
   }
+  const displayCaseOccupied = ()=>{
+    document.querySelector(".CaseOccupied").style.display = "flex";
+    const menuCaseOccupied = document.querySelector("#CaseOccupied");
+    menuCaseOccupied.addEventListener('click',()=>{
+      
+      document.querySelector(".CaseOccupied").style.display = "none";})
+
+  }
+
   const displayName = (name1,name2) =>{
     document.querySelector("#nameP1").textContent=name1;
     document.querySelector("#nameP2").textContent=name2;
@@ -311,7 +400,7 @@ const display = (()=>{
   const displayRound =(currentRound,Nbround)=>{
     document.querySelector("#roundcount").textContent="ROUND" +currentRound+"/"+Nbround;
   }
-  return {displayMainMenu,displayEndRoundMenu,displayEndGameMenu,displayName,displayScore,displayRound}
+  return {displayMainMenu,displayEndRoundMenu,displayEndGameMenu,displayName,displayScore,displayRound,displayCaseOccupied}
 })(); 
 display.displayMainMenu();
 
